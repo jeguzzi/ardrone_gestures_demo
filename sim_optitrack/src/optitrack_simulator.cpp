fd1c0abad9aba303c8f20b726b16a96e1dd367be
juan-pablo777 <juanpablo.rodriguezgomez@gmail.com>
2015-01-13 15:37:24 +0100

    changes on sim_optitrack

--- sim_optitrack/src/optitrack_simulator.cpp
+++ sim_optitrack/src/optitrack_simulator.cpp
@@ -2,59 +2,54 @@
 #include <string>
 #include <ros/ros.h>
 #include <gazebo_msgs/GetModelState.h>
-#include <sim_optitrack_msgs/SimRigidBodyData.h>
-
+#include <optitrack_msgs/RigidBodyData.h>
+#include <optitrack_msgs/RigidBodies.h>
 
 
 int main(int argc, char** argv){
     
-ros::init(argc, argv, "optitrack_simulator");
+ros::init(argc, argv, "SimOptitrack_node");
 ros::NodeHandle nh_;
 ros::Rate rate(120.0);
 
-ros::Publisher SimOptitrack_message = nh_.advertise<sim_optitrack_msgs::SimRigidBodyData>("sim_optitrack", 1);
-sim_optitrack_msgs::SimRigidBodyData data;
-
-int body_id;
-int mean_error = 0.0;
-std::string body_name;
-
-
-nh_.param("body_id",body_id,1);
-nh_.param<std::string>("body_name",body_name,"quadrotor");
-
+ros::Publisher SimOptitrack_message_ = nh_.advertise<optitrack_msgs::RigidBodies>("/optitrack/rigid_bodies", 1);
+std::vector<std::string> bodies_Vec;
   
 while (nh_.ok()){
 
+	nh_.getParam("bodies_list", bodies_Vec);
 	gazebo_msgs::GetModelState getmodelstate;
-	getmodelstate.request.model_name=body_name;
 
-	//Call ros_gazebo service    
-	ros::ServiceClient gmscl = nh_.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
-	gmscl.call(getmodelstate);
+	optitrack_msgs::RigidBodies rigid_bodies_msg;
+	//rigid_bodies_msg.header = header;
 	
-	// Id 
-	data.id = body_id;
-	data.name = body_name;	
-	data.mean_error = mean_error;
+	for(unsigned i=0; i < bodies_Vec.size(); i++) {
 
-	// Position
-	data.pose.position.x = getmodelstate.response.pose.position.x;
-	data.pose.position.y = getmodelstate.response.pose.position.y;
-	data.pose.position.z = getmodelstate.response.pose.position.z;
+		getmodelstate.request.model_name=bodies_Vec[i];
+		//Call ros_gazebo service    
+		ros::ServiceClient gmscl = nh_.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+		gmscl.call(getmodelstate);
 
-	// Twist 
-	
-	data.twist.linear.x = getmodelstate.response.twist.linear.x;
-	data.twist.linear.y = getmodelstate.response.twist.linear.y;
-	data.twist.linear.z = getmodelstate.response.twist.linear.z;
-	data.twist.angular.x = getmodelstate.response.twist.angular.x;
-	data.twist.angular.y = getmodelstate.response.twist.angular.y;
-	data.twist.angular.z = getmodelstate.response.twist.angular.z; 
+		optitrack_msgs::RigidBodyData data;
+		// Id 
+		data.id = i;
+		data.name = bodies_Vec[i];	
+		data.mean_error =0.0;
 
-	//std::cout << "Received position " << data.pose.position.x  << std::endl;
-	
-	SimOptitrack_message.publish(data);
+		// Position
+		data.pose.position.x = getmodelstate.response.pose.position.x;
+		data.pose.position.y = getmodelstate.response.pose.position.y;
+		data.pose.position.z = getmodelstate.response.pose.position.z;
+
+		data.pose.orientation.x = getmodelstate.response.pose.orientation.x;
+		data.pose.orientation.y = getmodelstate.response.pose.orientation.y;
+		data.pose.orientation.z = getmodelstate.response.pose.orientation.z;
+		data.pose.orientation.w = getmodelstate.response.pose.orientation.w;
+
+		rigid_bodies_msg.rigid_bodies.push_back(data);
+		}
+	//std::cout << "Orientation " << getmodelstate.response.pose.orientation.w  << std::endl;
+	SimOptitrack_message_.publish(rigid_bodies_msg);
 
 	ros::spinOnce();
 	rate.sleep();
